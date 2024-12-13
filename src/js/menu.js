09f128879cadd7d124f1e295b03330747046bb8f
francois-yang <francois.yang@edu.univ-fcomte.fr>
2024-12-13 14:30:28 +0100

    CSS de la page contact

--- src/js/menu.js
+++ src/js/menu.js
@@ -3,17 +3,17 @@ const menuClose = document.getElementById('menuClose');
 const menu = document.getElementById('mainNav');
 
 menuToggle.addEventListener('click', () => {
-    console.log('Menu toggle clicked'); // Log when the menu toggle button is clicked
+    console.log('Menu toggle clicked'); 
     menu.classList.add('menu--active');
 });
 
 menuClose.addEventListener('click', () => {
-    console.log('Menu close clicked'); // Log when the menu close button is clicked
+    console.log('Menu close clicked');
     menu.classList.remove('menu--active');
 });
 
 document.querySelectorAll('.menu__link').forEach(link => {
     link.addEventListener('click', (event) => {
-        console.log(`Menu item clicked: ${event.target.textContent}`); // Log which menu item was clicked
+        console.log(`Menu item clicked: ${event.target.textContent}`); 
     });
 });
